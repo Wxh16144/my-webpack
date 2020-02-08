e3fd3b14415d70e16576b5969e3b3f031fb3e068
Wuxh <wxh16144@qq.com>
2020-02-08 18:30:46 +0800

    feat: 添加react解析

--- src/search.js
+++ src/search.js
@@ -1,2 +1,11 @@
 
-document.write('Search Page')
+import React from 'react'
+import ReactDOM from 'react-dom'
+
+class Search extends React.Component {
+  render() {
+    return <div> Search Page </div>
+  }
+}
+
+ReactDOM.render(<Search />, document.getElementById('root'))
