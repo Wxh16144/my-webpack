bf414a4c16579d4e208a5889292b56540e0d5e0c
Wuxh <wxh16144@qq.com>
2020-02-08 18:20:00 +0800

    feat: 添加多个入口文件

---
+++ src/search.js
@@ -0,0 +1,2 @@
+
+document.write('Search Page')
--- webpack.config.js
+++ webpack.config.js
@@ -3,10 +3,13 @@ const path = require('path')
 module.exports = {
   mode: 'production',
   // default: ./src/index.js
-  entry: './src/index.js',
+  entry: {
+    index: './src/index.js',
+    search: './src/search.js'
+  },
   //  default: ./dist/main.js
   output: {
     path: path.join(__dirname, 'dist'),
-    filename: 'bundle.js'
+    filename: '[name].bundle.js'
   },
 }
