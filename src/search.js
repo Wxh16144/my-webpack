f5aa26b8f38c5e86c51e201bdbf110ae751c7d0d
Wuxh <wxh16144@qq.com>
2020-02-08 18:34:21 +0800

    feat:添加解析*.css文件

--- src/search.js
+++ src/search.js
@@ -2,9 +2,11 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 
+import './search.css'
+
 class Search extends React.Component {
   render() {
-    return <div> Search Page </div>
+    return <div className="search-text"> Search Page </div>
   }
 }
 
