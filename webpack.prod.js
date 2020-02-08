1f6d1ec2e05d43e4a764f9c72fb14345c9d35651
Wuxh <wxh16144@qq.com>
2020-02-08 20:27:58 +0800

    feat: 添加文件指纹 hash
    Hash: 和整改项目的构建相关，只要项目的文件哟修改，整个项目构建的 hash 就会更改 ｜ Chunkhash: 和 webpack 打包的 chunk 有关，不同的 entry 会生成不同的
    chunkhash 值 ｜Contenthash: 根据文件内容来定义 hash ，文件内容不变，则 contenthash 不变

---
+++ webpack.prod.js
@@ -0,0 +1,69 @@
+const path = require('path')
+
+const MiniCssExtractPlugin = require('mini-css-extract-plugin')
+
+
+module.exports = {
+  mode: 'production',
+  // default: ./src/index.js
+  entry: {
+    index: './src/index.js',
+    search: './src/search.js'
+  },
+  //  default: ./dist/main.js
+  output: {
+    path: path.join(__dirname, 'dist'),
+    filename: '[name]_[chunkhash:8].bundle.js'
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        use: 'babel-loader'
+      },
+      {
+        test: /\.css$/,
+        use: [
+          MiniCssExtractPlugin.loader,
+          'css-loader'
+        ]
+      },
+      {
+        test: /\.less$/,
+        use: [
+          MiniCssExtractPlugin.loader,
+          'css-loader',
+          'less-loader'
+        ]
+      },
+      {
+        test: /\.(png|jpg|jpeg|gif)$/,
+        use: {
+          loader: 'file-loader',
+          options: {
+            name: '[name]_[hash:8].[ext]'
+          }
+        }
+      },
+      {
+        test: /.(woff|woff2|eot|ttf|otf)$/,
+        use: {
+          loader: 'file-loader',
+          options: {
+            name: '[name]_[hash:8].[ext]'
+          }
+        }
+      }
+    ]
+  },
+  plugins: [
+    new MiniCssExtractPlugin({
+      filename: '[name]_[contenthash:8].css'
+    }),
+  ]
+}
+/* 
+Hash: 和整改项目的构建相关，只要项目的文件哟修改，整个项目构建的 hash 就会更改
+Chunkhash: 和 webpack 打包的 chunk 有关，不同的 entry 会生成不同的 chunkhash 值
+Contenthash: 根据文件内容来定义 hash ，文件内容不变，则 contenthash 不变
+*/
