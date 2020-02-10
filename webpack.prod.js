21241602b454862b61e76add9d88019b75da9415
Wuxh <wxh16144@qq.com>
2020-02-10 16:51:24 +0800

    feat: 添加eslint
    使用eslint-config-airbnb规则

--- webpack.prod.js
+++ webpack.prod.js
@@ -49,7 +49,10 @@ module.exports = {
     rules: [
       {
         test: /\.js$/,
-        use: 'babel-loader'
+        use: [
+          'babel-loader',
+          // 'eslint-loader'
+        ],
       },
       {
         test: /\.css$/,
