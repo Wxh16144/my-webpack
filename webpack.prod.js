8c84968516dc8b90a351e94a304bb0e767f51fa7
wuxh <wxh1220@gmail.com>
2023-04-09 15:40:42 +0800

    chore: ignore image-webpack-loader
    已经跑不起来了，不知道具体问题。我用的是 macos

--- webpack.prod.js
+++ webpack.prod.js
@@ -116,30 +116,30 @@ module.exports = {
             name: '[name]_[hash:8].[ext]',
           },
         },
-        {
-          loader: 'image-webpack-loader',
-          options: {
-            mozjpeg: {
-              progressive: true,
-              quality: 65,
-            },
-            // optipng.enabled: false will disable optipng
-            optipng: {
-              enabled: false,
-            },
-            pngquant: {
-              quality: [0.65, 0.90],
-              speed: 4,
-            },
-            gifsicle: {
-              interlaced: false,
-            },
-            // the webp option will enable WEBP
-            webp: {
-              quality: 75,
-            },
-          },
-        },
+        // {
+        //   loader: 'image-webpack-loader',
+        //   options: {
+        //     mozjpeg: {
+        //       progressive: true,
+        //       quality: 65,
+        //     },
+        //     // optipng.enabled: false will disable optipng
+        //     optipng: {
+        //       enabled: false,
+        //     },
+        //     pngquant: {
+        //       quality: [0.65, 0.90],
+        //       speed: 4,
+        //     },
+        //     gifsicle: {
+        //       interlaced: false,
+        //     },
+        //     // the webp option will enable WEBP
+        //     webp: {
+        //       quality: 75,
+        //     },
+        //   },
+        // },
         ],
       },
       {
